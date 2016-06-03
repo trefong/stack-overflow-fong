92da5db4475e1a46444b7371ec6176174c007bfd
Melissa C <email": null>
2016-06-03 14:10:18 -0700

    Fix seeds

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -2,6 +2,11 @@ get '/questions/new' do
   erb :'questions/new'
 end
 
-post 'questions/new' do
+post '/questions' do
   Question.create(title: params[:title], body: params[:body], user_id: session[:user_id])
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  erb :'questions/show'
 end
